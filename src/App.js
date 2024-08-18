0bb5bec482d89355b8c4c80eefebe64cad2bf860
Wairioko <charlesmungai5@gmail.com>
2024-08-18 19:26:56 +0300

    Addition of components to readBlog posts and updateblogs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import GetBlogs from './DisplayBlogs.js';
 import UserSignUp from './signup.js';
 import UserLogin from './login.js';
 import CreateBlog from './create-blog.js';
+import BlogPost from './readBlog.js';
+import UpdateBlog from './updateBlog.js';
+// import ReadBlog from './readBlog.js';
 
 
 function App() {
@@ -11,11 +14,14 @@ function App() {
     
     <Router>
             <Navbar/>
+      
             <Routes>
                 <Route path="/" element={<GetBlogs />} />
                 <Route path="/signup" element={<UserSignUp />} />
                 <Route path="/login" element={<UserLogin />} />
                 <Route path="/create-blog" element={<CreateBlog />} />
+                <Route path="/blog/:id" element={<BlogPost />} />
+                <Route path="/update-blog/:id" element={<UpdateBlog />} />
             </Routes>
     </Router>
     
