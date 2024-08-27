a37a4c4b41af09a14d5922b5e8b6b0cc3a3b2e26
Wairioko <charlesmungai5@gmail.com>
2024-08-27 21:46:02 +0300

    Wrapped navbar and routes in auth provider to check for expired jwtTokens and logout users for expired tokens

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthProvider  from './authProvider';
 import Navbar from './Navbar.js';
 import GetBlogs from './DisplayBlogs.js';
 import UserSignUp from './signup.js';
@@ -6,26 +8,29 @@ import UserLogin from './login.js';
 import CreateBlog from './create-blog.js';
 import BlogPost from './readBlog.js';
 import UpdateBlog from './updateBlog.js';
-// import ReadBlog from './readBlog.js';
 
+import ProfilePage from './profile.js';
+import ProfileChangePage from './edit-profile.js';
 
 function App() {
   return (
-    
     <Router>
-            <Navbar/>
-      
-            <Routes>
-                <Route path="/" element={<GetBlogs />} />
-                <Route path="/signup" element={<UserSignUp />} />
-                <Route path="/login" element={<UserLogin />} />
-                <Route path="/create-blog" element={<CreateBlog />} />
-                <Route path="/blog/:id" element={<BlogPost />} />
-                <Route path="/update-blog/:id" element={<UpdateBlog />} />
-            </Routes>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<GetBlogs />} />
+          <Route path="/signup" element={<UserSignUp />} />
+          <Route path="/login" element={<UserLogin />} />
+          <Route path="/create-blog" element={<CreateBlog />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
+          <Route path="/update-blog/:id" element={<UpdateBlog />} />
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/edit-profile' element={<ProfileChangePage />} />
+        </Routes>
+      </AuthProvider>
     </Router>
-    
   );
 }
 
 export default App;
+
