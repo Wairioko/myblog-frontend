015da379d340ec1292e5fcc3e2aa5a04b70a81c1
Wairioko <charlesmungai5@gmail.com>
2024-08-27 21:48:20 +0300

    Removed logout logic and moved it to authProvider where its deconstructed from

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,49 +1,40 @@
-import './index.css';
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from './authProvider';
 
 const Navbar = () => {
-    const token = localStorage.getItem('authToken');
-    const navigate = useNavigate();
-
-    const handleLogout = () => {
-        localStorage.removeItem('authToken');
-        localStorage.clear();
-
-        navigate('/login'); // Redirect to login page after logout
-    };
+    const { isAuthenticated, handleLogout } = useContext(AuthContext);
 
     return (
-            <nav>
-                <div className="nav-left">
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                    </ul>
-                </div>
-                <div className="nav-center">
-                    <Link to={'/'}>      
-                     <h1>MY BLOG</h1>
-                     </Link>
-  
-                </div>
-                <div className="nav-right">
-                    <ul>
-                        {!token && (
-                            <>
-                                <li><Link to="/login">Login</Link></li>
-                                <li><Link to="/signup">Sign Up</Link></li>
-                            </>
-                        )}
-                        {token && (
-                            <>
-                                <li><Link to="/profile">Profile</Link></li>
-                                <li><Link to="/create-blog">Create Blog</Link></li>
-                                <li><button onClick={handleLogout}>Logout</button></li>
-                            </>
-                        )}
-                    </ul>
-                </div>
-            </nav>
+        <nav>
+            <div className="nav-left">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                </ul>
+            </div>
+            <div className="nav-center">
+                <Link to={'/'}>
+                    <h1>MY BLOG</h1>
+                </Link>
+            </div>
+            <div className="nav-right">
+                <ul>
+                    {!isAuthenticated && (
+                        <>
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/signup">Sign Up</Link></li>
+                        </>
+                    )}
+                    {isAuthenticated && (
+                        <>
+                            <li><Link to="/profile">Profile</Link></li>
+                            <li><Link to="/create-blog">Create Blog</Link></li>
+                            <li><button onClick={handleLogout}>Logout</button></li>
+                        </>
+                    )}
+                </ul>
+            </div>
+        </nav>
     );
 }
 
