25576a6162ed878627e7603b612885b42a3e3973
Wairioko <charlesmungai5@gmail.com>
2024-09-04 16:15:27 +0300

    Improved display and error handling if no blogs

--- src/blogs/pages/GetAllBlogs.js
+++ src/blogs/pages/GetAllBlogs.js
@@ -6,34 +6,34 @@ import { Link } from "react-router-dom";
 const ITEMS_PER_PAGE = 3;
 
 const GetBlogs = () => {
-    const {blogs, loading, error}  = useGetAllBlogs();
+    const { blogs, loading, error } = useGetAllBlogs();
     const [currentPage, setCurrentPage] = useState(1);
 
-    if(loading) return <div>Loading...</div>
-    if(error) return <div>Error</div>
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>; 
 
-    if (blogs.length === 0) {
+    if (!blogs || blogs.length === 0) {
         return <div>No blogs available</div>;
     }
 
-    const startIndex = (currentPage -1) * ITEMS_PER_PAGE;
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
     const endIndex = startIndex + ITEMS_PER_PAGE;
     const currentBlogs = blogs.slice(startIndex, endIndex);
     const totalPages = Math.ceil(blogs.length / ITEMS_PER_PAGE);
 
     const handleNextPage = () => {
-        if(currentPage < totalPages){
+        if (currentPage < totalPages) {
             setCurrentPage(currentPage + 1);
         }
     };
 
     const handlePreviousPage = () => {
-        if(currentPage > 1){
+        if (currentPage > 1) {
             setCurrentPage(currentPage - 1);
         }
-    }
+    };
 
-    return (  
+    return (
         <div className="blogs-content">
             {currentBlogs.map((blog) => (
                 <Link to={`/blog/${blog._id}`} key={blog._id}>
@@ -46,19 +46,22 @@ const GetBlogs = () => {
             ))}
 
             <div className="pagination-controls">
-                <button name="previous" onClick={handlePreviousPage} disabled={currentPage === 1}>
+                <button
+                    name="previous"
+                    onClick={handlePreviousPage}
+                    disabled={currentPage === 1}
+                >
                     Previous
                 </button>
                 <span>Page {currentPage} of {totalPages}</span>
-                <button name="next"onClick={handleNextPage} disabled={currentPage === totalPages}>
+                <button
+                    name="next"
+                    onClick={handleNextPage}
+                    disabled={currentPage === totalPages}
+                >
                     Next
                 </button>
             </div>
         </div>
     );
-}
- 
-export default GetBlogs;
-
-
-
+};
