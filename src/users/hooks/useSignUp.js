c709c84afbf48c395a64458c0193e29234efa236
Wairioko <charlesmungai5@gmail.com>
2024-09-04 15:38:13 +0300

    navigation to home page after successful login

--- src/users/hooks/useSignUp.js
+++ src/users/hooks/useSignUp.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {UserSignUp} from "../services/userServices";
 
 const useSignUp = () => {
     const [email, setEmail] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const navigate = Navigate();
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
