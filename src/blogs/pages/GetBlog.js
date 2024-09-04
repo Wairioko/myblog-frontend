82ccc6b8a563b526ab27fab77f9e7282c6b4797a
Wairioko <charlesmungai5@gmail.com>
2024-09-04 07:02:42 +0300

    Component to display a blog

---
+++ src/blogs/pages/GetBlog.js
@@ -0,0 +1,70 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { UseGetBlog } from "../hooks/useGetBlog";
+import UseDeleteBlog from "../hooks/useDeleteBlog";
+import { useContext } from 'react';
+import { UserContext } from "../../users/userContexts/userContexts";
+
+const DisplayBlog = () => {
+  const { profile } = useContext(UserContext);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const token = localStorage.getItem('authToken');
+  const { blog, isLoading, error } = UseGetBlog(id);
+
+  const onDelete = async () => {
+      const success = await UseDeleteBlog(id, token);
+      if (success) {
+          navigate('/');
+      }
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!blog) return <div>No blog found</div>;
+
+  const canEditOrDelete = token && profile?.username === blog.author;
+
+  return (
+      <div className="blog-post">
+          <h1>{blog.title}</h1>
+          {blog.imageUrls && blog.imageUrls[0] && (
+              <img src={blog.imageUrls[0]} alt="" className="image-large" />
+          )}
+          <p className="author">Posted by {blog.author}</p>
+          <p className="description">{blog.description}</p>
+          <div className="content">
+              {blog.content && blog.content.split('\n\n').map((paragraph, index, arr) => {
+                  const middleIndex = Math.ceil(arr.length / 2) - 1;
+                  return (
+                      <div key={index} className="paragraph-container">
+                          <p>{paragraph}</p>
+                          {index === middleIndex && blog.imageUrls[1] && (
+                              <img
+                                  src={blog.imageUrls[1]}
+                                  alt=""
+                                  className="image-inline"
+                                  style={{ display: 'block', margin: '20px auto' }}
+                              />
+                          )}
+                      </div>
+                  );
+              })}
+          </div>
+          {canEditOrDelete && (
+              <div className="update-delete-container">
+                  <button name="delete" onClick={onDelete}>Delete Blog</button>
+                  <button
+                      name="update-blog"
+                      onClick={() => {
+                          navigate(`/update-blog/${blog._id}`, { state: { blog } });
+                      }}
+                  >
+                      Update Blog
+                  </button>
+              </div>
+          )}
+      </div>
+  );
+};
+
+export default DisplayBlog;
