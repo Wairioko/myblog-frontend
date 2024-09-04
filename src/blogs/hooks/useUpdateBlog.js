38c59a890babe0e88be5d11445a593a55b400007
Wairioko <charlesmungai5@gmail.com>
2024-09-04 06:59:12 +0300

    Hook to update blog details

---
+++ src/blogs/hooks/useUpdateBlog.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { updateBlog } from "../services/blogService";
+import { useNavigate, useParams } from "react-router-dom";
+
+export const useUpdateBlog = (blog = {}) => { // Add a default empty object for blog
+    const { id } = useParams(); 
+    const [title, setTitle] = useState(blog.title || '');
+    const [description, setDescription] = useState(blog.description || '');
+    const [content, setContent] = useState(blog.content || '');
+    const [images, setImages] = useState([]);
+    const [existingImageUrls, setExistingImageUrls] = useState(blog.imageUrls || []);
+    const [isLoading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    // useEffect(() => {
+    //     if (blog) { // Ensure blog is defined before setting states
+    //         setTitle(blog.title || '');
+    //         setDescription(blog.description || '');
+    //         setContent(blog.content || '');
+    //         setExistingImageUrls(blog.imageUrls || []);
+    //     }
+    // }, [blog]); // Re-run the effect if blog changes
+    
+    const handleSubmit = async (e) => {
+        
+        e.preventDefault();
+        setLoading(true);
+        
+        const newBlogData = {
+            title, description, content, images, imageUrls: existingImageUrls
+        };
+
+        try {
+            
+            await updateBlog(id, newBlogData);
+            
+
+        } catch (error) {
+            console.log(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleImageChange = (e) => {
+        const selectedFiles = Array.from(e.target.files);
+        if (selectedFiles.length + existingImageUrls.length > 2) {
+            alert("You can only have a maximum of 2 images in total");
+            return;
+        }
+        setImages(selectedFiles);
+    };
+    
+    const handleRemoveExistingImage = (urlToRemove) => {
+        setExistingImageUrls(existingImageUrls.filter(url => url !== urlToRemove));
+    };
+
+    return {
+        title,
+        content,
+        description,
+        setDescription,
+        setTitle,
+        setContent,
+        images,
+        handleImageChange,
+        handleRemoveExistingImage,
+        handleSubmit,
+        isLoading
+    };
+};
