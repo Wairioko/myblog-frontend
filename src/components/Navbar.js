e15bcef62336fc31df952abfdb320a767264aa93
Wairioko <charlesmungai5@gmail.com>
2024-09-06 15:24:30 +0300

    change to navbar and css change to reflect changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,47 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../authProvider';
-
 const Navbar = () => {
     const { isAuthenticated, handleLogout } = useContext(AuthContext);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <nav>
-            <div className="nav-left">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                </ul>
-            </div>
-            <div className="nav-center">
-                <Link to={'/'}>
-                    <h1>MY BLOG</h1>
-                </Link>
-            </div>
-            <div className="nav-right">
-                <ul>
-                    {!isAuthenticated && (
-                        <>
-                            <li><Link to="/login">Login</Link></li>
-                            <li><Link to="/signup">Sign Up</Link></li>
-                        </>
-                    )}
-                    {isAuthenticated && (
-                        <>
-                            <li><Link to="/profile">Profile</Link></li>
-                            <li><Link to="/create-blog">Create Blog</Link></li>
-                            <li><button onClick={handleLogout}>Logout</button></li>
-                        </>
-                    )}
-                </ul>
+            <div className="nav-container">
+                <div className="nav-left">
+                    <Link to="/">Home</Link>
+                </div>
+                <div className="nav-center">
+                    <Link to={'/'}>
+                        <h1>MY BLOG</h1>
+                    </Link>
+                </div>
+                <button className="nav-toggle" onClick={toggleMenu}>☰</button>
+                <div className={`nav-right ${isMenuOpen ? 'active' : ''}`}>
+                    <ul>
+                        {!isAuthenticated && (
+                            <>
+                                <li><Link to="/login">Login</Link></li>
+                                <li><Link to="/signup">Sign Up</Link></li>
+                            </>
+                        )}
+                        {isAuthenticated && (
+                            <>
+                                <li><Link to="/profile">Profile</Link></li>
+                                <li><Link to="/create-blog">Create Blog</Link></li>
+                                <li><button onClick={handleLogout}>Logout</button></li>
+                            </>
+                        )}
+                    </ul>
+                </div>
             </div>
         </nav>
     );
 }
+
 
 export default Navbar;
