d775be44e4356aa0b258ad9d0d7a86046afd902c
Wairioko <charlesmungai5@gmail.com>
2024-09-09 20:55:41 +0300

    removed id from parameters being passed inside editProfile  service function

--- src/users/hooks/useEditProfile.js
+++ src/users/hooks/useEditProfile.js
@@ -5,10 +5,9 @@ import { useState } from "react";
 
 export const useEditProfile = () => {
     const navigate = Navigate();
-    const id = useParams();
     const [username, setUsername] = useState('');
     const handleSubmit = () => {
-        EditProfile(id, username)
+        EditProfile(username)
         navigate('/profile')
     }
     return {username, setUsername, handleSubmit}
