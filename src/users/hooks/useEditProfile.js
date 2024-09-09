e1704e9276782a9412126d7038dd7990df674958
Wairioko <charlesmungai5@gmail.com>
2024-09-09 21:05:00 +0300

    Correct importation of useNavigate

--- src/users/hooks/useEditProfile.js
+++ src/users/hooks/useEditProfile.js
@@ -1,10 +1,10 @@
-import { Navigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import {EditProfile} from "../services/userServices";
 import { useState } from "react";
 
 
 export const useEditProfile = () => {
-    const navigate = Navigate();
+    const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const handleSubmit = () => {
         EditProfile(username)
